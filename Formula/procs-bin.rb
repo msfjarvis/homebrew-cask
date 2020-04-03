0862ec421d422962f37609293dfa1707b90db7fc
Harsh Shandilya <me@msfjarvis.dev>
2020-04-03 23:05:04 +0530

    procs-bin: Set conflict strategy for procs
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- Formula/procs-bin.rb
+++ Formula/procs-bin.rb
@@ -5,6 +5,8 @@ class ProcsBin < Formula
   url "https://github.com/dalance/procs/releases/download/v#{version}/procs-v#{version}-x86_64-lnx.zip"
   sha256 "40c992e70216440e4392cca7d9a9015d391fdfab567a31502fe976ca9288b582"
 
+  conflicts_with "procs"
+
   def install
     bin.install "procs"
   end
