4dd96f1033ed747848c12aacdbe367d7a3a9b886
Harsh Shandilya <me@msfjarvis.dev>
2020-04-03 22:40:13 +0530

    Add procs-bin formula
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ Formula/procs-bin.rb
@@ -0,0 +1,11 @@
+class ProcsBin < Formula
+  version "0.9.20"
+  desc "A modern replacement for ps written in Rust"
+  homepage "https://github.com/dalance/procs"
+  url "https://github.com/dalance/procs/releases/download/v#{version}/procs-v#{version}-x86_64-lnx.zip"
+  sha256 "40c992e70216440e4392cca7d9a9015d391fdfab567a31502fe976ca9288b582"
+
+  def install
+    bin.install "procs"
+  end
+end
