06d25957b7e2154099bee4486c53580f3ef6aa90
Harsh Shandilya <me@msfjarvis.dev>
2020-04-05 16:01:19 +0530

    Bump shellcheck-bin to 0.7.1
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- Formula/shellcheck-bin.rb
+++ Formula/shellcheck-bin.rb
@@ -1,9 +1,9 @@
 class ShellcheckBin < Formula
-  version "0.7.0"
+  version "0.7.1"
   desc "ShellCheck, a static analysis tool for shell scripts"
   homepage "https://shellcheck.net"
   url "https://github.com/koalaman/shellcheck/releases/download/v#{version}/shellcheck-v#{version}.linux.x86_64.tar.xz"
-  sha256 "39c501aaca6aae3f3c7fc125b3c3af779ddbe4e67e4ebdc44c2ae5cba76c847f"
+  sha256 "64f17152d96d7ec261ad3086ed42d18232fcb65148b44571b564d688269d36c8"
 
   conflicts_with "shellcheck"
 
