67b79cb21cd5317f5e916a576c61dd56a27539cf
Harsh Shandilya <me@msfjarvis.dev>
2020-04-05 23:04:33 +0530

    gopass-bin: Fix homepage URL
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- Formula/gopass-bin.rb
+++ Formula/gopass-bin.rb
@@ -1,7 +1,7 @@
 class GopassBin < Formula
   version "1.8.6"
   desc "The slightly more awesome standard unix password manager for teams"
-  homepage "gopass.pw"
+  homepage "https://github.com/gopasspw/gopass"
   url "https://github.com/gopasspw/gopass/releases/download/v#{version}/gopass-#{version}-linux-amd64.tar.gz"
   sha256 "c3f8eb2943d46c13c1c5818395b76ecbe71252fc53e0634d30e8038732baf33c"
 
